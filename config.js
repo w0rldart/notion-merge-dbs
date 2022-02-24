c83401f45bce26a2fc292112c27e53eef639ff55
Alex Budurovici <673081+w0rldart@users.noreply.github.com>
2022-02-24 18:03:14 +0100

    Added logging to file when log level is debug

--- config.js
+++ config.js
@@ -9,24 +9,34 @@ const argvs = minimist(process.argv.slice(2));
  * Advanced logging
  */
 export const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.combine(
-    winston.format.prettyPrint({ colorize: true })
-  ),
-  transports: [new winston.transports.Console()],
+  level: process.env.LOG_LEVEL || "info",
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.prettyPrint({ colorize: true })
+      ),
+    }),
+    new winston.transports.File({
+      filename: 'debug.log',
+      level: 'debug',
+      format: winston.format.combine(
+        winston.format.prettyPrint({ colorize: false })
+      ),
+    }),
+  ],
 });
 
 /**
  * Source database or databases to process
  */
-export const SOURCE_DATABASES = argvs.source || JSON.parse(process.env.SOURCE_DATABASES);
+ export const SOURCE_DATABASES = argvs.source || JSON.parse(process.env.SOURCE_DATABASES);
 
-/**
- * Target database where to inster the records from SOURCE_DATABASES
- */
-export const TARGET_DB = argvs.target || process.env.TARGET_DB;
-
-/**
- * Initialise Notion SDK Client
- */
-export const notion = new Client({ auth: process.env.NOTION_API_KEY });
+ /**
+  * Target database where to inster the records from SOURCE_DATABASES
+  */
+ export const TARGET_DB = argvs.target || process.env.TARGET_DB;
+ 
+ /**
+  * Initialise Notion SDK Client
+  */
+ export const notion = new Client({ auth: process.env.NOTION_API_KEY });
